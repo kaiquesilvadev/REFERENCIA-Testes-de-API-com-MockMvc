8f82f39d5465f235e3963f8209926984f5b9eaa5
kaiquesilvadev <j.kaique1570@gmail.com>
2024-02-24 19:48:15 -0300

    Inserção de produto insere produto com dados válidos quando logado como admin

--- src/test/java/com/devsuperior/dscommerce/TI/ProductControllerTI.java
+++ src/test/java/com/devsuperior/dscommerce/TI/ProductControllerTI.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dscommerce.TI;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -13,7 +14,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.entities.Product;
+import com.devsuperior.dscommerce.fabricaOBJ.criaProduct;
+import com.devsuperior.dscommerce.util.TokenUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -23,26 +31,58 @@ public class ProductControllerTI {
 	@Autowired
 	private MockMvc mockMvc;
 	private String productNome;
-	
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@Autowired
+	private TokenUtil tokenUtil;
+
+	private String ClienteToken, adminToken, ivalidoToken;
+	private Product product;
+	private ProductDTO productDTO;
+
 	@BeforeEach
 	void setUp() throws Exception {
-		productNome ="Macbook";
-		 
+		// token
+		adminToken = tokenUtil.obtainAccessToken(mockMvc, "alex@gmail.com", "123456");
+		
+		//var
+		product = criaProduct.novoProduct();
+		productNome = "Macbook";
+
 	}
-	
+
 	@DisplayName("get Deve Retorna Macbook Quando Status For Ok")
 	@Test
 	public void getDeveRetornaMacbookQuandoStatusForOk() throws Exception {
-		
-		   ResultActions resultado = mockMvc
-				   .perform(get("/products?size=12&page=0&name={productNome}" , productNome)
-						   .accept(MediaType.APPLICATION_JSON));
-		   
-		   resultado.andExpect(status().isOk());
-		   resultado.andExpect(jsonPath("$.content[0].id").value(3L));
-		   resultado.andExpect(jsonPath("$.content[0].name").value("Macbook Pro"));
-		   resultado.andExpect(jsonPath("$.content[0].price").value( 1250.0));
-		   resultado.andExpect(jsonPath("$.content[0].imgUrl")
-				   .value( "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg"));
+
+		ResultActions resultado = mockMvc.perform(
+				get("/products?size=12&page=0&name={productNome}", productNome).accept(MediaType.APPLICATION_JSON));
+
+		resultado.andExpect(status().isOk());
+		resultado.andExpect(jsonPath("$.content[0].id").value(3L));
+		resultado.andExpect(jsonPath("$.content[0].name").value("Macbook Pro"));
+		resultado.andExpect(jsonPath("$.content[0].price").value(1250.0));
+		resultado.andExpect(jsonPath("$.content[0].imgUrl").value(
+				"https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg"));
 	}
- }
+
+	@DisplayName("")
+	@Test
+	public void tets() throws Exception {
+
+		productDTO = new ProductDTO(product);
+		String body = objectMapper.writeValueAsString(productDTO);
+
+		 ResultActions resultado = mockMvc.perform(
+				 post("/products")
+				.header("Authorization", "Bearer " + adminToken)
+				.content(body)
+				.contentType(MediaType.APPLICATION_JSON) 
+				.accept(MediaType.APPLICATION_JSON))
+				.andDo(MockMvcResultHandlers.print());
+
+		 resultado.andExpect(status().isCreated());
+	}
+}
